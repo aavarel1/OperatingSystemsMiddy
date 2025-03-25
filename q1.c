61cda6b1b174033d4201c7cf19df05659c7737d8
aavarel1 <112527875+aavarel1@users.noreply.github.com>
2025-03-24 19:23:06 -0500

    s

---
+++ q1.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main()
+{
+    // Open a file for writing (create if it doesn't exist)
+    int fd = open("example.txt", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    if (fd == -1)
+    {
+        perror("Error opening file");
+        return 1;
+    }
+
+    // Fork to create a new process
+    pid_t pid = fork();
+
+    if (pid == -1)
+    {
+        perror("Fork failed");
+        return 1;
+    }
+
+    if (pid == 0)
+    {
+        // Child process writing to the file
+        const char *child_msg = "Child process writing.\n";
+        if (write(fd, child_msg, 22) == -1)
+        {
+            perror("Error writing by child");
+        }
+        close(fd);
+        return 0;
+    }
+    else
+    {
+        // Parent process writing to the file
+        const char *parent_msg = "Parent process writing.\n";
+        if (write(fd, parent_msg, 22) == -1)
+        {
+            perror("Error writing by parent");
+        }
+        close(fd);
+        return 0;
+    }
+}
